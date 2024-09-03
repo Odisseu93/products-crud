00ca406ac4955143d6b39c713f20b7f3ce09292d
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-09-03 20:16:30 -0300

    feat: add startup settings to sqlite3 library

--- app/back-end/src/libs/sqlite.ts
+++ app/back-end/src/libs/sqlite.ts
@@ -1,5 +1,29 @@
 import sqlite3 from 'sqlite3'
 import { Database } from 'sqlite'
-export { sqlite3, Database }
+import path from 'path'
+import { db } from '../db'
+
+export const queries = [
+  'DROP TABLE IF EXISTS products',
+  'CREATE TABLE IF NOT EXISTS "products" ( id TEXT NOT NULL, name TEXT NOT NULL, price REAL NOT NULL, PRIMARY KEY(id))',
+]
+
+const sqlFile = path.join(__dirname, '../db/database.sql')
+
+const sqliteDb = new Database({
+  filename: sqlFile,
+  // filename: ':memory:',
+  driver: sqlite3.Database,
+})
+
+const config = {
+  execute: async () => {
+    await sqliteDb.open()
+    await Promise.all(queries.map(query => db.exec(query)))
+    await sqliteDb.close()
+  },
+}
 
 //https://www.npmjs.com/package/sqlite
+
+export { sqliteDb, config }

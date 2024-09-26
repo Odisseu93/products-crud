6e48119d5c9bc2c760bfdb2b002fa8f02b661baa
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-09-26 18:09:17 -0300

    feat: add updateProductControllerFactory

---
+++ app/back-end/src/factories/product/update-product-controller-factory.ts
@@ -0,0 +1,12 @@
+import { UpdateProductController } from '../../controllers/product/update-product-contoller'
+import { ProductRepositorySqlite } from '../../repositories/sqlite/products-repository-sqlite'
+import { UpdateProductService } from '../../services/product/update-product-service'
+import { controllerAdapter } from 'vkrun'
+
+export const updateProductControllerFactory = () => {
+  const repository = new ProductRepositorySqlite()
+  const service = new UpdateProductService(repository)
+
+  const controller = new UpdateProductController(service)
+  return controllerAdapter(controller)
+}

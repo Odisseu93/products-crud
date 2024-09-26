39bb5d113f14e9adeb2ca4482d113a6637fc9006
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-09-26 18:06:03 -0300

    feat: add more schemas to validate request body and parameters

--- app/back-end/src/utils/schemas.ts
+++ app/back-end/src/utils/schemas.ts
@@ -1,8 +1,25 @@
 import { schema as vs } from 'vkrun'
+import { VkrunCors } from 'vkrun/lib/modules/cors'
 
 export const createProductSchema = vs().object({
   body: vs().object({
-    name: vs().alias('nome do produto').string(),
-    price: vs().alias('preço').number().min(0),
+    name: vs().string(),
+    price: vs().number().min(0),
+  }),
+})
+
+export const updateProductSchema = vs().object({
+  params: vs().object({
+    id: vs().string(),
+  }),
+  body: vs().object({
+    name: vs().string(),
+    price: vs().number().min(0),
+  }),
+})
+
+export const ProductIdSchema = vs().object({
+  params: vs().object({
+    id: vs().string(),
   }),
 })

521e35b25a1cbf6555e85c6816e1f63b1d065683
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-09-26 18:20:56 -0300

    fix: add all product related routes

--- app/back-end/src/routes/index.ts
+++ app/back-end/src/routes/index.ts
@@ -1,6 +1,14 @@
-import { createProductSchema } from '../utils/schemas'
+import {
+  createProductSchema,
+  ProductIdSchema,
+  updateProductSchema,
+} from '../utils/schemas'
 import { Router, validateRouteData } from 'vkrun'
 import { createProductControllerFactory } from '../factories/product/create-product-controller-factory'
+import { deleteProductControllerFactory } from '../factories/product/delete-product-controller-factory'
+import { getAllProductsControllerFactory } from '../factories/product/get-all-products-controller-factory'
+import { updateProductControllerFactory } from '../factories/product/update-product-controller-factory'
+import { getProductControllerFactory } from '../factories/product/get-product-controller-factory'
 
 const router = Router()
 
@@ -8,6 +16,26 @@ router.post(
   '/product/create',
   validateRouteData(createProductSchema),
   createProductControllerFactory()
+)
+
+router.delete(
+  '/product/delete/:id',
+  validateRouteData(ProductIdSchema),
+  deleteProductControllerFactory()
+)
+
+router.get('/product/get/all', getAllProductsControllerFactory())
+
+router.put(
+  '/product/update/:id',
+  validateRouteData(updateProductSchema),
+  updateProductControllerFactory()
+)
+
+router.get(
+  '/product/get/:id',
+  validateRouteData(ProductIdSchema),
+  getProductControllerFactory()
 )
 
 export { router }

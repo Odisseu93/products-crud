1ebc4e3783970c742e7c657966d594575b68355c
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-09-26 18:08:33 -0300

    feat: add deleteProductControllerFactory

---
+++ app/back-end/src/factories/product/delete-product-controller-factory.ts
@@ -0,0 +1,12 @@
+import { DeleteProductController } from '../../controllers/product/delete-product-contoller'
+import { ProductRepositorySqlite } from '../../repositories/sqlite/products-repository-sqlite'
+import { DeleteProductService } from '../../services/product/delete-product-service'
+import { controllerAdapter } from 'vkrun'
+
+export const deleteProductControllerFactory = () => {
+  const repository = new ProductRepositorySqlite()
+  const service = new DeleteProductService(repository)
+
+  const controller = new DeleteProductController(service)
+  return controllerAdapter(controller)
+}

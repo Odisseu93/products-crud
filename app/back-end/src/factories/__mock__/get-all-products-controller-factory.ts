f02fabf939aa66e132f81f427a9ead50591b1a50
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-11-16 19:09:27 -0300

    feat: add mocks for product factories

---
+++ app/back-end/src/factories/__mock__/get-all-products-controller-factory.ts
@@ -0,0 +1,13 @@
+import { GetAllProductsService } from '../../services/product/get-all-products-service'
+import { ProductRepositorySqlite } from '../../repositories/sqlite/__mock__/products-repository-sqlite'
+import { controllerAdapter } from 'vkrun'
+import { GetAllProductsController } from '../../controllers/product/get-all-products-contoller'
+
+export const getAllProductsControllerFactory = () => {
+  const repository = new ProductRepositorySqlite()
+  const service = new GetAllProductsService(repository)
+
+  const controller = new GetAllProductsController(service)
+  const adpter = controllerAdapter(controller)
+  return adpter
+}

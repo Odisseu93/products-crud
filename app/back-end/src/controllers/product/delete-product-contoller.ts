6cad7b50e669a9c7598595765746fed2b15f6116
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2024-11-16 19:21:32 -0300

    feat: add typing for controller parameters: "GetProductController", "DeleteProductController" and "UpdateProductController"

--- app/back-end/src/controllers/product/delete-product-contoller.ts
+++ app/back-end/src/controllers/product/delete-product-contoller.ts
@@ -1,7 +1,7 @@
 import { DeleteProductServiceInterface } from '../../interfaces/services/product/delete-product-interface'
 import { Controller, Response, Request } from 'vkrun'
 
-
+type DeleteProductParameters = { id: string }
 export class DeleteProductController implements Controller {
   private deleteProductService: DeleteProductServiceInterface
   constructor(deleteProductService: DeleteProductServiceInterface) {
@@ -9,9 +9,9 @@ export class DeleteProductController implements Controller {
   }
 
   public async handle(req: Request, res: Response) {
-    const id = req.params?.id as string
+    const params = req.params as DeleteProductParameters
 
-    const output = await this.deleteProductService.execute({id})
+    const output = await this.deleteProductService.execute({ id: params.id })
 
     res.status(output.statusCode).json(output.content)
   }

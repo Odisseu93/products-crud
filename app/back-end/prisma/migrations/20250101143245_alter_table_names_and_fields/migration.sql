e73b395d28a6829e67af05e2b1457606b586c18a
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-02-05 19:58:44 -0300

    add warehouse and product_warehouse tables

---
+++ app/back-end/prisma/migrations/20250101143245_alter_table_names_and_fields/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the `wharehouse` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "wharehouse";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "warehouses" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_product_warehouse" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "productId" TEXT NOT NULL,
+    "warehouseId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    CONSTRAINT "product_warehouse_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "product_warehouse_warehouseId_fkey" FOREIGN KEY ("warehouseId") REFERENCES "warehouses" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_product_warehouse" ("id", "productId", "quantity", "warehouseId") SELECT "id", "productId", "quantity", "warehouseId" FROM "product_warehouse";
+DROP TABLE "product_warehouse";
+ALTER TABLE "new_product_warehouse" RENAME TO "product_warehouse";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
